16f8fb6b5653ca56b4ecbe669953e84e7ad6c1bc
Jing <jtan1993@gmail.com>
2020-08-19 08:15:14 +0100

    update sort

--- listingObjects.py
+++ listingObjects.py
@@ -1,9 +1,15 @@
 from maya import cmds
-selection = print cmds.ls(selection = True)
+selection = cmds.ls(selection = True)
 
-if len(selection) == 0: #get a new selection
+'''get a new selection'''
+if len(selection) == 0: 
 	selection = cmds.ls(dag = True, long = True) 
 
-#sort using the length of the objects because children will have a longer full name: act on them first to not affect their path
-selection.Sort(key = len, reverse = True) 
-print selection
+'''sort using the length of the objects because children will have a longer full name: act on them first to not affect their path'''
+selection.sort(key = len, reverse = True) 
+'''print selection'''
+
+'''sort using the length of the objects because children will have a longer full name: act on them first to not affect their path'''
+for obj in selection:
+	shortName = obj.split("|")[-1] 
+	print shortName
