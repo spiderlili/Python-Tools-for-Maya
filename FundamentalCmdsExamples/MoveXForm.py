6e91af9bfb75718485d6edfd02c85ac669bcf3bb
Jing <jtan1993@gmail.com>
2020-12-18 18:33:58 +0000

    import pymel

--- FundamentalCmdsExamples/MoveXForm.py
+++ FundamentalCmdsExamples/MoveXForm.py
@@ -9,18 +9,19 @@ mc.xform(myCube, r = True, t = (5,5,5))
 # get translation in relative & world space using query
 mc.xform(myCube, ws = True, t = True, q = True)
 
-# set rotation relative and os space 
+# set rotation relative and os space in euler angles, may cause gimbal rotation issues when using euler angles & 2 axis are on top of each other
 mc.xform(myCube, eu = True, ro = (45,45,45))
 mc.xform(myCube, r = True, eu = True, ro = (45,90,120))
 mc.xform(myCube, ws = True, eu = True, ro = (45,45,45))
 
-# gimbal same visible rotation but matrix values differ
+# gimbal lock: same visible rotation but matrix values differ
 mc.xform(myCube, ws = True, eu = True, ro = (45,90,120))
 mc.xform(myCube, eu = True, ro = (45,90,120))
 
 # get rotation
 mc.xform(myCube, ws = True, ro = True, q = True)
 
+import pymel.core as pm
 pos1 = pm.selected()[0].getMatrix()
 pos2 = pm.selected()[0].getMatrix()
 pos2 == pos1
