aab7be54c47f6fe550ec149f4eb19f5de63697ac
Jing <jtan1993@gmail.com>
2020-12-24 16:49:59 +0000

    mel2py

--- FundamentalCmdsExamples/MelToPy.py
+++ FundamentalCmdsExamples/MelToPy.py
@@ -1,29 +1,13 @@
-import maya.cmds as mc
+import pymel.core as pymel
+import pymel.tools.mel2py as mel2py
+result = pymel.promptDialog(title = 'Convert Mel to Pymel', message = 'Enter Mel string: ', button = ['OK','Cancel'], defaultButton = 'OK', cancelButton = 'Cancel', dismissText = 'Cancel')
 
-myCube = mc.polyCube(n = 'MyCube')[0]
+if result == 'OK':
+	text = pymel.promptDialog(query = True, text = True)
 
-# set translation in relative & world space
-mc.xform(myCube, ws = True, t = (5,5,5))
-mc.xform(myCube, r = True, t = (5,5,5))
+	#get the pymel equivalent
+	pymelAnswer = mel2py.mel2pyStr(text, pymelNamespace = 'mc')
 
-# get translation in relative & world space using query
-mc.xform(myCube, ws = True, t = True, q = True)
-
-# set rotation relative and os space in euler angles, may cause gimbal rotation issues when using euler angles & 2 axis are on top of each other
-mc.xform(myCube, eu = True, ro = (45,45,45))
-mc.xform(myCube, r = True, eu = True, ro = (45,90,120))
-mc.xform(myCube, ws = True, eu = True, ro = (45,45,45))
-
-# gimbal lock: same visible rotation but matrix values differ
-mc.xform(myCube, ws = True, eu = True, ro = (45,90,120))
-mc.xform(myCube, eu = True, ro = (45,90,120))
-
-# get rotation
-mc.xform(myCube, ws = True, ro = True, q = True)
-
-import pymel.core as pm
-pos1 = pm.selected()[0].getMatrix()
-
-# no visible differences in matrices but not the same
-mc.xform(myCube, ws = True, eu = True, ro = (45, 90, 120))
-mc.xform(myCube, eu = True, ro = (45, 90, 120))
+	#get rid of the old way
+	pymelCode = pymelAnswer.replace('pymel.all', 'maya.cmds')
+	print(pymelCode)
