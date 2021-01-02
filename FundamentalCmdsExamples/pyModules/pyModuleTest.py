2580034b3866e8e2309152e46f6aab762e3f4f33
Jing <jtan1993@gmail.com>
2021-01-02 14:40:36 +0000

    module test

--- FundamentalCmdsExamples/pyModules/pyModuleTest.py
+++ FundamentalCmdsExamples/pyModules/pyModuleTest.py
@@ -1,9 +1,10 @@
+""" module file test: run import sys sys.path.append('moduleFolderPath'), then run import moduleName """
+
 import sys
 sys.path
 
 def sayHello():
 	print("Hello!")
-sayHello()
 
 def makeStep():
 	""" makes a step """
@@ -11,5 +12,3 @@ def makeStep():
 	step,stepHist = mc.polyCube(name = "step", ch = 1)
 	mc.setAttr(stepHist + ".h", 0.5)
 	mc.setAttr(stepHist + ".depth", 5.0)
-
-makeStep()
