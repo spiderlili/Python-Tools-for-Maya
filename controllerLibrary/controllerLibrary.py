6ffd34eda04ce3221c8130e8ed4c74a1aea11d22
Jing Chun Tan <jtan1993@gmail.com>
2021-04-16 21:46:16 +0100

    create dir

--- controllerLibrary/controllerLibrary.py
+++ controllerLibrary/controllerLibrary.py
@@ -2,3 +2,19 @@ from maya import cmds
 import os
 import json
 import pprint
+
+USER_APP_DIR = cmds.internalVar(userAppDir = True) # user's maya app directory
+DIRECTORY = os.path.join(USER_APP_DIR, 'controllerLibrary') # use os specific separator (different between Windows/Mac/Linux)
+
+# if function is not given any value: it will use the default directory
+def createDirectory(directory = DIRECTORY):
+	"""
+	Creates the given directory if it doesn't exist already
+	Args:
+	directory(str): the directory to create
+	"""
+
+	# if this directory does not exist: make this directory
+	if not os.path.exists(directory):
+		os.mkdir(directory)
+
