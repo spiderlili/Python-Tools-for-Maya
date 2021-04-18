10d1bcd62502510e374c91720ca5eabae2ef6e58
Jing Chun Tan <jtan1993@gmail.com>
2021-04-18 13:57:35 +0100

    find saved controllers

--- controllerLibrary/controllerLibrary.py
+++ controllerLibrary/controllerLibrary.py
@@ -1,5 +1,3 @@
-# goal: create the library directory
-
 from maya import cmds
 import os
 import json
@@ -8,7 +6,7 @@ import pprint
 USER_APP_DIR = cmds.internalVar(userAppDir = True) # user's maya app directory
 DIRECTORY = os.path.join(USER_APP_DIR, 'controllerLibrary') # use os specific separator (different between Windows/Mac/Linux)
 
-# if function is not given any value: it will use the default directory
+# create the library directory. if function is not given any value: it will use the default directory
 def createDirectory(directory = DIRECTORY):
 	"""
 	Creates the given directory if it doesn't exist already
@@ -31,3 +29,19 @@ class ControllerLibrary(dict):
 			cmds.file(force = True, type = 'mayaAscii', exportedSelected = True)
 		else:
 			cmds.file(save = True, type = 'mayaAscii', force = True) # if the file already exists: force save over it
+	
+	# method to find any controller .ma files that has been saved
+	def find(self, directory = DIRECTORY):
+		if not os.path.exists(directory):
+			return
+
+		files = os.listdir(directory)
+		mayaFiles = [mayafile for mayafile in files if mayafile.endswith('.ma')] # avoid using file keyword in python
+
+		# loop through maya files & add them to the dictionary, linking to the file path
+		for ma in mayaFiles:
+			name, extension = os.path.splitext(ma) # split the extension from the file, give back name & extension - just print file name without extension
+			path = os.path.join(directory, ma)
+			self[name] = path # inherit from dict: can access cells as if it's a dictionary
+
+		pprint.pprint(self) # print key & value in a pretty easy to read manner
