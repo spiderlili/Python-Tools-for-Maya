69be4b568a16f73b38f201867c9c0c3cba8c02ee
Jing Chun Tan <jtan1993@gmail.com>
2021-04-19 22:01:05 +0100

    init ui

--- controllerLibrary/controllerLibraryUI.py
+++ controllerLibrary/controllerLibraryUI.py
@@ -1,1 +1,6 @@
 from controllerLibrary import ControllerLibrary # import the ControllerLibrary class from controllerLibrary.py
+from Qt import QtWidgets, QtCore, QtGui # QtWidgets does not exist in pre-2016 Maya
+
+class ControllerLibraryUI(QtWidgets.QDialog): # ControllerLibrary is a dialog inherited from QDialog
+	def __init__(self):
+		super(ControllerLibraryUI, self).__init__() # same as: QtWidgets.QDialog.__init__(self) but more flexible & scalable, can inherit from multiple classes
