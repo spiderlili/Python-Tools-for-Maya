73d04ed82cb1d4f087bb2c0f231ce6cc0f82abdf
Jing Chun Tan <jtan1993@gmail.com>
2021-04-20 21:36:02 +0100

    display ui window

--- controllerLibrary/controllerLibraryUI.py
+++ controllerLibrary/controllerLibraryUI.py
@@ -4,3 +4,11 @@ from Qt import QtWidgets, QtCore, QtGui # QtWidgets does not exist in pre-2016 M
 class ControllerLibraryUI(QtWidgets.QDialog): # ControllerLibrary is a dialog inherited from QDialog
 	def __init__(self):
 		super(ControllerLibraryUI, self).__init__() # same as: QtWidgets.QDialog.__init__(self) but more flexible & scalable, can inherit from multiple classes
+		self.setWindowTitle('Controller Library UI') 
+		self.library = ControllerLibrary() # Store an instance of ControllerLibrary inside UI
+		self.buildUI()
+		self.populate()
+
+def showUI():
+	ui = ControllerLibraryUI() # create new instance
+	ui.show()
