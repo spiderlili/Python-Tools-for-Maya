8e8eea337d3d6335463a60b50c5a3747052c8220
Jing Chun Tan <jtan1993@gmail.com>
2021-04-20 21:59:21 +0100

    qt ui building

--- controllerLibrary/controllerLibraryUI.py
+++ controllerLibrary/controllerLibraryUI.py
@@ -9,6 +9,35 @@ class ControllerLibraryUI(QtWidgets.QDialog): # ControllerLibrary is a dialog in
 		self.buildUI()
 		self.populate()
 
+	def buildUI(self):
+		layout = QtWidgets.QVBoxLayout(self) # create master vertical box layout (maya column layout)
+		saveWidget = QtWidgets.QWidget() # base class of all UI objects for the top save layout: save button & text field
+		saveLayout = QtWidgets.QHBoxLayout(saveWidget) # horizontal box layout for saveWidget
+		layout.addWidget(saveWidget)
+
+		self.saveNameField = QtWidgets.QLineEdit() # use self to access the saveNameField later: variable accessible inside of class
+		saveLayout.addWidget(self.saveNameField) # add text field to the saveWidget horizontal box layout 
+
+		saveBtn = QtWidgets.QPushButton('Save')
+		saveLayout.addWidget(saveBtn)
+
+		self.listWidget = QtWidgets.QListWidget() # use self to access the list of thumbnails later
+		layout.addWidget(self.listWidget)
+
+		btnWidget = QtWidgets.QWidget()  # base class of all UI objects for the bottom horizontal buttons layout group
+		btnLayout = QtWidgets.QHBoxLayout(btnWidget)
+		layout.addWidget(btnWidget)
+		importBtn = QtWidgets.QPushButton('Import')
+		btnLayout.addWidget(importBtn)
+		refreshBtn = QtWidgets.QPushButton('Refresh')
+		btnLayout.addWidget(refreshBtn)
+		closeBtn = QtWidgets.QPushButton('Close')
+		btnLayout.addWidget(closeBtn)
+
+	def populate(self):
+		pass
+
 def showUI():
 	ui = ControllerLibraryUI() # create new instance
 	ui.show()
+	return ui
