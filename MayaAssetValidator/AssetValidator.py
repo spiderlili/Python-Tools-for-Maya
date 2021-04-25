5b621c418111f485b81120fc3167a8c334dcfc3b
Jing <jtan1993@gmail.com>
2021-04-25 18:14:36 +0100

    updated if statements

--- MayaAssetValidator/AssetValidator.py
+++ MayaAssetValidator/AssetValidator.py
@@ -1,44 +1,22 @@
 import maya.cmds as cmds
 
-class Gear(object):
-    def __init__(self):
-        # set default values
-        self.transform = None
-        self.extrudeFaces = None
-        self.constructor = None
-
-    def createGear(self, teeth=10, length = 0.3):
-        """
-        create a gear with the specified parameters:
-        Args:
-            teeth: the number of teeth to create
-            length: the length of teeth
-
-        Returns:
-            a tuple of the constructor, transform, polyExtrude node
-        """
-        spans = teeth * 2
-        self.transform, self.constructor = cmds.polyPipe(subdivisionsAxis = spans)
-        sideFaces = range(spans * 2, spans * 3, 2)
-
-        cmds.select(clear=True)
-        for face in sideFaces:
-            cmds.select('%s.f[%s]' %(self.transform, face), add = True)
-
-        self.extrudeFaces = cmds.polyExtrudeFacet(localTranslateZ=length)[0]
-
-    def changeTeeth(self, teeth=10, length=0.3):
-        spans = teeth * 2
-        # use polypipe with the poly pipe that you already have. use edit flag so it knows to edit the existing one instead of creating a new one
-        cmds.polyPipe(self.constructor, edit=True, subdivisionsAxis=spans)
-        sideFaces = range(spans * 2, spans * 3, 2)
-        faceNames = []
-
-        for face in sideFaces:
-            faceName = 'f[%s]' % (face)
-            faceNames.append(faceName)
-
-        # print faceNames
-        # get the length of the faceNames list, give it to setAttr(), get list of all the faces to use: expand the face names list so each one will be a parameter
-        cmds.setAttr('%s.inputComponents' % (self.extrudeFaces), len(faceNames), *faceNames, type="componentList")
-        cmds.polyExtrudeFacet(self.extrudeFaces, edit=True, ltz=length)  # localTranslateZ shorthand
+def validateMesh():
+	"""
+	Validate the mesh in Maya
+	"""
+	for obj in cmds.listRelatives(cmds.ls(type = mesh, v = True), p = True, fullPath = True):
+		validityFlags = ' mesh: %s\n' % obj
+		if not cmds.polyInfo(obj, invalidEdges = True):
+			validityFlags += ' invalid edges found\n'
+		if not cmds.polyInfo(obj, invalidVertices = True):
+			validityFlags += ' invalid vertices found\n'
+		if not cmds.polyInfo(obj, laminaFaces = True):
+			validityFlags += ' lamina faces found\n'
+		if not cmds.polyInfo(obj, nonManifoldEdges = True):
+			validityFlags += ' non-manifold faces found\n'
+		if not cmds.polyInfo(obj, nonManifoldUVEdges = True):
+			validityFlags += ' non-manifold UV edges found\n'			
+		if not cmds.polyInfo(obj, nonManifoldUVs = True):
+			validityFlags += ' non-manifold UV found\n'	
+		if not cmds.polyInfo(obj, nonManifoldVertices = True):
+			validityFlags += ' non-manifold vertices found\n'	
