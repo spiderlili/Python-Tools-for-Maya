25f18013e217de9a7053d262a6a8e460483efb8d
Jing Chun Tan <jtan1993@gmail.com>
2021-05-18 23:02:44 +0100

    naming change

--- AlembicsExporter/AlembicsExporter.py
+++ AlembicsExporter/AlembicsExporter.py
@@ -1,6 +1,6 @@
 import maya.cmds as cmds
 
-def exportMesh(scene):
+def exportAbcMesh(scene):
 	"""
 	Export mesh
 	"""
@@ -10,14 +10,21 @@ def exportMesh(scene):
 		meshes = cmds.listRelatives(cmds.ls(type='mesh', v=True), p=True, fullPath=True)
 		cmds.select(meshes)
 
+		# string which returns the long root path to the mesh name
 		abcStr = ''
 		for obj in cmds.ls(sl=True, l=True):
-			abcStr += '-root ' + obj
+			abcStr += ' -root ' + obj
 
 		cmds.loadPlugin("AbcExport.so")
-		start,end = 1,1
 
-		export = scene.replace('.ma', '.abc')
-		abcStr = '%s -frameRange %s %s -uvWrite -file %s' % (abcStr, start, end, export)
+		# export mesh animation: min & max time of the frame range
+		start, end = cmds.playbackOptions(q=True, minTime=True), cmds.playbackOptions(q=True, maxTime=True) 
+
+		# export static mesh: 1 single frame
+		start, end = 1,1
+
+		# swap the maya file for abc
+		exportPath = scene.replace('.ma', '.abc')
+		abcStr = '%s -frameRange %s %s -uvWrite -file %s' % (abcStr, start, end, exportPath)
 		cmds.AbcExport(j = abcStr)
-		return export
+		return exportPath
