3822f0d2474de72f5f0fcae69dc75b6b0421aafd
Jing Tan <jtan1993@gmail.com>
2022-07-28 09:14:25 +0100

    Update fileListerExecuter.py

--- FileLister/fileListerExecuter.py
+++ FileLister/fileListerExecuter.py
@@ -1,4 +1,6 @@
-import sys
+# TODO: Copy this code to userSetup.py to auto-run this script everytime the user starts Maya
+import maya.cmds as cmds
+import os, sys
 import importlib
 sys.path.append("/Users/jingtan/Documents/GitHub/Python-Tools-for-Maya/FileLister")
 
