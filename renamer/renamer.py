c0102ff9d937094efb5de504a43daa555b3851f7
Jing Tan <jtan1993@gmail.com>
2022-08-16 13:39:26 +0100

    Update renamer.py

--- renamer/renamer.py
+++ renamer/renamer.py
@@ -1,15 +1,21 @@
 import maya.cmds as cmds
 
 # TOOD - Prevent error: cannot rename a read only node
-def addSuffix():
+def addPrefixOrSuffix(type):
     selection = cmds.ls(sl = True)
     if len(selection) > 0:
-        cmds.promptDialog(t = "Add Suffix", m = "Enter Suffix Name: ", button = ["OK", "Cancel"], db = "OK", cb = "Cancel", dismissString = "Cancel")
-        suffixText = cmds.promptDialog(q = True, text = True)
-        if suffixText != "":
-            for object in selection:
-                cmds.rename(object, object + "_"+ suffixText) 
+        cmds.promptDialog(t = "Add Prefix/Suffix", m = "Enter Name: ", button = ["OK", "Cancel"], db = "OK", cb = "Cancel", dismissString = "Cancel")
+        text = cmds.promptDialog(q = True, text = True)
+        
+        if text != "":
+            if type == "prefix":
+                for object in selection:
+                    cmds.rename(object, text + "_" + object) 
+            if type == "suffix":
+                for object in selection:
+                    cmds.rename(object, object + "_" + text) 
+        
     else:
-        cmds.promptDialog(t = "Error", m = "No object selected! Please select at least 1 object.")
+        cmds.confirmDialog(t = "Error", m = "No object selected! Please select at least 1 object.", button = ["OK"], cb = "OK", dismissString = "OK")
             
-addSuffix()       
+addPrefixOrSuffix("prefix")       
