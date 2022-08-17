72460682b222f9bcfc775329d73a41a0c77229fb
Jing Tan <jtan1993@gmail.com>
2022-08-17 14:30:15 +0100

    add ui

--- renamer/renamer.py
+++ renamer/renamer.py
@@ -18,7 +18,7 @@ def addPrefixOrSuffix(type):
     else:
         cmds.confirmDialog(t = "Error", m = "No object selected! Please select at least 1 object.", button = ["OK"], cb = "OK", dismissString = "OK")
             
-addPrefixOrSuffix("prefix")       
+# addPrefixOrSuffix("prefix")       
 
 def UI():
     if cmds.window("renamerUI", exists = True):
@@ -26,12 +26,22 @@ def UI():
     window = cmds.window("renamerUI", w = 300, h = 150, title = "Renamer", mxb = False, sizeable = False)
     
     # Create the layout
-    layuot = cmds.formLayout()
+    layout = cmds.formLayout()
     
     # Create prefix & suffix checkboxes
     prefixCheckBox = cmds.checkBox("prefixCheckBox", label = "Add Prefix", v = False)
     suffixCheckBox = cmds.checkBox("suffixCheckBox", label = "Add Suffix", v = False)
-    prefixText = cmds.textField("prefixTextField", w = 200)
-    suffixText = cmds.textField("suffixTextField", w = 200)
+    prefixLabel = cmds.text(label = "Prefix: ")
+    prefixText = cmds.textField("prefixTextField", w = 200, enable = False)
+    suffixLabel = cmds.text(label = "Suffix: ")
+    suffixText = cmds.textField("suffixTextField", w = 200, enable = False)
+    
+    # Layout the items
+    cmds.formLayout(layout, edit = True, af = [(prefixCheckBox, "left", 10), (prefixCheckBox, "top", 10)])
+    cmds.formLayout(layout, edit = True, af = [(suffixCheckBox, "left", 100), (suffixCheckBox, "top", 10)])
+    cmds.formLayout(layout, edit = True, af = [(prefixText, "left", 50), (prefixText, "top", 40)])
+    cmds.formLayout(layout, edit = True, af = [(suffixText, "left", 50), (suffixText, "top", 70)])
+    cmds.formLayout(layout, edit = True, af = [(prefixLabel, "left", 10), (prefixLabel, "top", 40)])
+    cmds.formLayout(layout, edit = True, af = [(suffixLabel, "left", 10), (suffixLabel, "top", 70)])
     
     cmds.showWindow(window)
