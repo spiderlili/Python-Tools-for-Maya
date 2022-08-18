af64c48eb49a5d401f36a5b4f21c5de69f525162
Jing Tan <jtan1993@gmail.com>
2022-08-18 19:50:35 +0100

    set up buttons

--- renamer/renamer.py
+++ renamer/renamer.py
@@ -1,4 +1,5 @@
 import maya.cmds as cmds
+from functools import partial
 
 # TOOD - Prevent error: cannot rename a read only node
 def addPrefixOrSuffix(type):
@@ -20,6 +21,25 @@ def addPrefixOrSuffix(type):
             
 # addPrefixOrSuffix("prefix")       
 
+def checkBoxChanged(type, *args):
+    # Get the value of the passed in checkbox
+    # print (args)
+    checkboxValue = cmds.checkBox(type + "CheckBox", q = True, v = True)
+    if checkboxValue == True:
+        cmds.textField(type + "TextField", edit = True, enable = True)
+    if checkboxValue == False:
+        cmds.textField(type + "TextField", edit = True, enable = False)
+        
+
+def accept(*args):
+    prefix = cmds.textField("prefixTextField", q = True, text = True)
+    suffix = cmds.textField("suffixTextField", q = True, text = True)
+    print (prefix)
+    print (suffix)
+
+def cancel(*args):
+    cmds.deleteUI("renamerUI")
+
 def UI():
     if cmds.window("renamerUI", exists = True):
         cmds.deleteUI("renamerUI")
@@ -29,12 +49,15 @@ def UI():
     layout = cmds.formLayout()
     
     # Create prefix & suffix checkboxes
-    prefixCheckBox = cmds.checkBox("prefixCheckBox", label = "Add Prefix", v = False)
-    suffixCheckBox = cmds.checkBox("suffixCheckBox", label = "Add Suffix", v = False)
+    prefixCheckBox = cmds.checkBox("prefixCheckBox", label = "Add Prefix", v = False, cc = partial(checkBoxChanged, "prefix"))
+    suffixCheckBox = cmds.checkBox("suffixCheckBox", label = "Add Suffix", v = False, cc = partial(checkBoxChanged, "suffix"))
     prefixLabel = cmds.text(label = "Prefix: ")
     prefixText = cmds.textField("prefixTextField", w = 200, enable = False)
     suffixLabel = cmds.text(label = "Suffix: ")
     suffixText = cmds.textField("suffixTextField", w = 200, enable = False)
+    
+    acceptButton = cmds.button(label = "Accept", w = 130, command = accept)
+    cancelButton = cmds.button(label = "Cancel", w = 130, command = cancel)
     
     # Layout the items
     cmds.formLayout(layout, edit = True, af = [(prefixCheckBox, "left", 10), (prefixCheckBox, "top", 10)])
@@ -43,5 +66,8 @@ def UI():
     cmds.formLayout(layout, edit = True, af = [(suffixText, "left", 50), (suffixText, "top", 70)])
     cmds.formLayout(layout, edit = True, af = [(prefixLabel, "left", 10), (prefixLabel, "top", 40)])
     cmds.formLayout(layout, edit = True, af = [(suffixLabel, "left", 10), (suffixLabel, "top", 70)])
+    
+    cmds.formLayout(layout, edit = True, af = [(acceptButton, "left", 10), (acceptButton, "bottom", 10)])
+    cmds.formLayout(layout, edit = True, af = [(cancelButton, "left", 150), (cancelButton, "bottom", 10)])
     
     cmds.showWindow(window)
