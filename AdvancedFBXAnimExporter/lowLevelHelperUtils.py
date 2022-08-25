20da5881c4fb7824906d4d30cbd6d80a8067ac23
Jing Tan <jtan1993@gmail.com>
2022-08-25 16:25:51 +0100

    Update lowLevelHelperUtils.py

--- AdvancedFBXAnimExporter/lowLevelHelperUtils.py
+++ AdvancedFBXAnimExporter/lowLevelHelperUtils.py
@@ -9,5 +9,14 @@ def TagForOrigin(node):
         cmds.addAttr(node, shortName = "org", longName = "origin", at = "bool")
         cmds.setAttr(node + ".origin", True)
         
-TagForOrigin("locator1")        
-TagForOrigin("pCone1")        
+# Add attributes to the mesh so the exporter can find them
+# If the object exists & attribute does not exist: add the exportMeshes message attribute
+def TagForMeshExport(mesh):
+    if cmds.objExists(mesh) and not cmds.objExists(mesh + ".exportMeshes"):
+        cmds.addAttr(mesh, shortName = "xms", longName = "exportMeshes", at = "message")
+        
+# Add attributes to the node so the exporter can find export definitions
+# If the object exists & attribute does not exist: add the exportNode message attribute
+def TagForExportNode(node):
+    if cmds.objExists(node) and not cmds.objExists(node + ".exportNode"):
+        cmds.addAttr(node, shortName = "xnd", longName = "exportNode", at = "message")
